272ccc03612e899cda4498aa8009336ab5765993
qwwang <chu8129@users.noreply.github.com>
2021-02-01 11:22:32 +0800

    Create 26_taobao_search_crawl.sh

---
+++ 26_taobao_search_crawl.sh
@@ -0,0 +1,13 @@
+curl -fsSL https://get.docker.com -o get-docker.sh
+sudo sh get-docker.sh
+sudo usermod -aG docker qw
+sudo docker pull scrapinghub/splash
+# docker run -d -p 8050:8050 scrapinghub/splash
+
+sudo docker run -d -p 8050:8050 --memory=2G --restart=always scrapinghub/splash --maxrss 4000
+scrapy startproject taobao
+cd taobao
+scrapy genspider taobao_goods taobao.com
+
+
+## 代码不公开了，一堆干无意义事情的
