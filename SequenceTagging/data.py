65548efd6f721346fae9b3af91c31e6a98507ce7
wangqiuwen <wangqiuwen@youmi.net>
2021-09-07 18:45:20 +0800

    update

--- SequenceTagging/data.py
+++ SequenceTagging/data.py
@@ -3,13 +3,13 @@
 # Create By qiuwen(mail:chu8129@gmail.com) @ 2021-08-31 18:38:37
 
 
-
 import sys
+
 sys.path.append("./")
 
 import logging
-logger = logging.getLogger("root")
 
+logger = logging.getLogger("root")
 
 
 import keras
@@ -21,7 +21,9 @@ import json
 
 
 class TextGenerator(keras.utils.Sequence):
-    def __init__(self, batch_size, vocab_max=100000):
+    def __init__(self, batch_size, max_length=128, vocab_max=100000):
+
+        self.max_length = max_length
         self.batch_size = batch_size
         self.vocab_size_max = vocab_max
 
@@ -39,47 +41,41 @@ class TextGenerator(keras.utils.Sequence):
     def init(self, file_path, train=True):
 
         with open(file_path) as fr:
-            for line in fr.readlines()[:100]:
-                data = line.strip("\n").split("\t", 2)
-                self.data_list.append((self.cut_word(data[2]), data[1]))
+            for line in fr:
+                data = line.strip("\n").split("\t")
+                self.data_list.append((self.cut_word(data[1]), data[0]))
 
         self.data_size = len(self.data_list)
 
         if not train:
             return
 
-        label_set = reduce(lambda data_set, content:set(self.split(content)) | data_set, map(lambda cells:cells[1], iter(self.data_list)), set())
-        self.label_to_index = dict(_[::-1] for _ in enumerate(sorted(vocab_set),start=1))
+        label_set = reduce(
+            lambda data_set, content: set(content.strip()) | data_set,
+            map(lambda cells: cells[1], iter(self.data_list)),
+            set(),
+        )
+        self.label_to_index = dict(_[::-1] for _ in enumerate(sorted(label_set), start=0))
         self.index_to_label = dict(_[::-1] for _ in self.label_to_index.items())
-        self.label_size = max(self.index_to_label.keys())
+        self.label_size = max(self.index_to_label.keys()) + 1
 
         word_set = dict(
-            reduce(
-                lambda s1, s2: s1 + s2,
-                map(lambda string: Counter(string[0].split()), self.data_list),
-            ).most_common(self.vocab_size_max)
+            reduce(lambda s1, s2: s1 + s2, map(lambda string: Counter(string[0].split()), self.data_list),).most_common(
+                self.vocab_size_max
+            )
         )
-        self.vocab_size = len(word_set)
-        self.vocab_to_index = dict(zip(word_set, range(self.vocab_size)))
-        self.index_to_vocab = dict(zip(range(self.vocab_size), word_set))
+        self.vocab_to_index = dict(_[::-1] for _ in enumerate(sorted(word_set), start=1))
+        self.index_to_vocab = dict(_[::-1] for _ in self.vocab_to_index.items())
+        self.vocab_size = max(self.index_to_vocab.keys()) + 1
 
         with open("params", "w") as fw:
-            fw.write(
-                json.dumps(
-                    [
-                        self.vocab_to_index,
-                        self.index_to_vocab,
-                        self.label_to_index,
-                        self.index_to_label,
-                    ]
-                )
-            )
+            fw.write(json.dumps([self.vocab_to_index, self.index_to_vocab, self.label_to_index, self.index_to_label,]))
 
     def cut_word(self, line):
         return " ".join(jieba.cut(line))
 
     def __len__(self):
-        return self.data_size / self.batch_size
+        return int(self.data_size / self.batch_size) + 1
 
     def __getitem__(self, index):
         sentence_label_list = self.data_list[index * self.batch_size : (index + 1) * self.batch_size]
@@ -87,16 +83,23 @@ class TextGenerator(keras.utils.Sequence):
         label_list = [_[1] for _ in sentence_label_list]
         return self.transform_data(sentence_list, label_list)
 
+    def split(self, text):
+        return list(jieba.lcut(text))
+
     def transform_data(self, sentence_list, label_list):
-        x_vec = numpy.zeros([len(sentence_list), self.vocab_size], dtype=numpy.uint8)
+        x_vec = numpy.zeros([len(sentence_list), self.max_length], dtype=numpy.uint8)
+        y_vec = numpy.zeros([len(label_list), self.max_length, self.label_size], dtype=numpy.uint8)
+
         for index, sentence in enumerate(sentence_list):
             x_words = [word for word in sentence.split()]
-            word_indexs = [self.vocab_to_index[word] for word in x_words if word in self.vocab_to_index]
-            for word_index in word_indexs:
-                x_vec[index][word_index] = 1
+            onehotindex_charindex_list = [
+                (self.vocab_to_index[word], index) for index, word in enumerate(x_words) if word in self.vocab_to_index
+            ]
+            for onehot_index, char_index in onehotindex_charindex_list:
+                x_vec[index][char_index] = onehot_index
 
-        y_vec = numpy.zeros([len(label_list), self.label_size])
-        for index, label in enumerate(label_list):
-            label_index = self.label_to_index[label]
-            y_vec[index][label_index] = 1
+        for index, labels in enumerate(label_list):
+            for char_index, label in enumerate(labels):
+                label_value = self.label_to_index[label]
+                y_vec[index][char_index][label_value] = 1
         return x_vec, y_vec
--- SequenceTagging/lstm.py
+++ SequenceTagging/lstm.py
@@ -3,43 +3,84 @@
 # Create By qiuwen(mail:chu8129@gmail.com) @ 2021-08-31 17:37:32
 
 
-
 import sys
+
 sys.path.append("./")
 
 import logging
+
 logger = logging.getLogger("root")
 
 import tensorflow
+import tensorflow_addons
 from data import TextGenerator
+
 
 class BiLstmCrf:
     def build(self):
-        model = tensorflow.keras.models.Sequential()
-        model.add(tensorflow.keras.layers.Embedding(self.vocab_size, self.embedding_size))
-        model.add(tensorflow.keras.layers.Bidirectional(tensorflow.keras.layers.LSTM(self.tag_size, return_sequence=True, active="tanh"), merge_mode="sum"))
-        model.add(tensorflow.keras.layers.Bidirectional(tensorflow.keras.layers.LSTM(self.tag_size, return_sequence=True, active="softmax"), merge_mode="sum"))
+        input_ = tensorflow.keras.Input(shape=(self.max_length,))
+        embedding_ = tensorflow.keras.layers.Embedding(
+            self.vocab_size, self.embedding_size, input_length=self.max_length, mask_zero=True
+        )(input_)
+        lstm_1_ = tensorflow.keras.layers.Bidirectional(
+            tensorflow.keras.layers.LSTM(
+                self.tag_size, return_sequences=True, recurrent_dropout=0.6, activation="tanh"
+            ),
+            merge_mode="sum",
+        )(embedding_)
+        lstm_ = tensorflow.keras.layers.Bidirectional(
+            tensorflow.keras.layers.LSTM(
+                self.tag_size, return_sequences=True, recurrent_dropout=0.6, activation="softmax"
+            ),
+            merge_mode="sum",
+        )(lstm_1_)
         # https://www.tensorflow.org/addons/api_docs/python/tfa/layers/CRF
-        import  tensorflow_addons
-        model.add(tensorflow_addons.layers.CRF(self.tag_size, name="crf")[0])
-        model.compile(tensorflow.keras.optimizers.Adam(learning_rate=self.learning_rate, loss={"crf": crf.get_loss}))
+        decoded_sequence, potentials, sequence_length, chain_kernel = tensorflow_addons.layers.CRF(
+            self.tag_size, name="crf"
+        )(lstm_)
+        model = tensorflow.keras.Model(input_, potentials)
+        model.compile(
+            optimizer=tensorflow.keras.optimizers.RMSprop(learning_rate=0.3),
+            loss=tensorflow_addons.losses.SigmoidFocalCrossEntropy(),
+        )
+        print(model.summary())
         self.model = model
 
     def __init__(self):
-        data_generater = TextGenerator(128).init("data")
+        self.batch_size = 128
+        self.max_length = 10
+        self.epochs = 100
 
-        self.vocab_dict = data_generater.vocab_to_index
-        self.tag_dict = data_generater.label_to_index
+        self.data_generater = TextGenerator(self.batch_size, self.max_length)
+        self.data_generater.init("data.tsv")
 
-        self.vocab_size = max(self.vocab_dict.values())
-        self.embedding_size = 128
-        self.tag_size = max(self.tag_dict.values())
+        self.max_length = self.data_generater.max_length
+
+        self.vocab_dict = self.data_generater.vocab_to_index
+        self.tag_dict = self.data_generater.label_to_index
+
+        self.vocab_size = self.data_generater.vocab_size
+        self.tag_size = self.data_generater.label_size
+
+        self.embedding_size = 64
 
         self.model = None
         self.build()
 
+    def fit(self):
+        for _ in range(self.epochs):
+            for index in range(len(self.data_generater)):
+                x, y = self.data_generater[index]
+                self.model.fit(
+                    tensorflow.convert_to_tensor(x),
+                    tensorflow.convert_to_tensor(y),
+                    epochs=1,
+                    batch_size=self.batch_size,
+                    callbacks=[tensorflow.keras.callbacks.TensorBoard(log_dir="logs", histogram_freq=1)],
+                )
+        self.model.save("finalmodel.h5")
 
-    def fit(self, X, y, epochs=100, transParam=None):
-        return self.model.fit(X, y, epochs=epochs, callbacks=[tensorflow.keras.callbacks.TensorBoard(log_dir="logs", histogram_freq=1)])
 
-
+if __name__ == "__main__":
+    model = BiLstmCrf()
+    model.fit()
