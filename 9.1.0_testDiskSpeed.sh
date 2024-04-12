da7a528a9723b393416b93409a6cb2e954345399
qiuwen.wang <chu8129@users.noreply.github.com>
2024-04-12 16:50:06 +0800

    Update 9.1.0_testDiskSpeed.sh

--- 9.1.0_testDiskSpeed.sh
+++ 9.1.0_testDiskSpeed.sh
@@ -1,1 +1,3 @@
 dd if=/dev/zero of=./test.iso bs=1024M count=1
+dd if=/root/autodl-fs/qwcache/testfile of=/dev/null bs=1G count=1 iflag=direct
+dd if=/dev/zero of=/root/autodl-fs/qwcache/testfile bs=1G count=1 oflag=direct
