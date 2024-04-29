498861e9e3795cc5c670d00464ae18df2e9bd17a
qiuwen.wang <chu8129@users.noreply.github.com>
2024-04-29 16:54:19 +0800

    Update 9.9_huggingfaceDownload.sh

--- 9.9_huggingfaceDownload.sh
+++ 9.9_huggingfaceDownload.sh
@@ -1,11 +1,21 @@
 repoid=${1}
-path=`realpath ./${2}`
-filename=${3}
-repoType=${4}
-echo $path
+`mkdir -p ${repoid}`
+
+path=`realpath ./${repoid}`
+echo "path:${path}"
+
+repoTypeDefault="model"
+repoType=${2:-${repoTypeDefault}}
+echo "repo type:${repoType}"
+
+fileMatch="*"
+filename=${3:-${fileMatch}}
+echo "filename:${filename}"
+
 strings="from huggingface_hub import hf_hub_download, snapshot_download;snapshot_download(token='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', repo_type='${repoType}', repo_id='${repoid}', force_download=False, resume_download=False, local_dir='${path}', cache_dir='${path}', local_dir_use_symlinks=False, allow_patterns='${filename}');"
 echo ${strings}
+
 export HF_ENDPOINT=https://hf-mirror.com
 `python -c "${strings}" `
 
-# bash **download.sh repoid repoid "*" model/data
+# bash **download.sh repoid model/data "*"
