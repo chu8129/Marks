93d52d1add54389b4998378c702e9050156e4782
qiuwen.wang <chu8129@users.noreply.github.com>
2024-05-22 12:03:31 +0800

    Update installNvidiaDriver.sh

--- dockerfile/installNvidiaDriver.sh
+++ dockerfile/installNvidiaDriver.sh
@@ -10,6 +10,9 @@ sudo service lightdm stop
 sudo service docker stop
 sudo rmmod nvidia_uvm
 sudo service nvidia-persistenced stop
+
+#wget https://us.download.nvidia.com/XFree86/Linux-x86_64/550.78/NVIDIA-Linux-x86_64-550.78.run
+sudo apt install build-essential libglvnd-dev pkg-config
 filename=./NVIDIA-Linux-x86_64-535.146.02.run
 chmod +x ${filename}
 sudo ${filename}
