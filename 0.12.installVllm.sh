da24a37717efe28f9b20985d3189964b1548b94a
qiuwen.wang <chu8129@users.noreply.github.com>
2024-07-01 16:32:50 +0800

    Update 0.12.installVllm.sh

--- 0.12.installVllm.sh
+++ 0.12.installVllm.sh
@@ -1,5 +1,5 @@
-
 TMPDIR=/dev/shm pip3 install --cache-dir=/dev/shm -U torch==2.2.1 torchvision==0.17.1 torchaudio==2.2.1 --index-url https://download.pytorch.org/whl/cu121
+TMPDIR=/dev/shm pip3 install --cache-dir=/dev/shm -U pip install torchsde
 TMPDIR=/dev/shm pip3 install --cache-dir=/dev/shm -U pip install vllm==0.4.1
 TMPDIR=/dev/shm pip3 install --cache-dir=/dev/shm -U git+https://github.com/huggingface/transformers.git
 TMPDIR=/dev/shm pip3 install --cache-dir=/dev/shm -U git+https://github.com/huggingface/peft.git
