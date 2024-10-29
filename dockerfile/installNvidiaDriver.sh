4080d8ed995f4d076bdea8038de8c3e82fa2f8d5
qiuwen.wang <chu8129@users.noreply.github.com>
2024-10-29 13:54:34 +0800

    add-ucloud

--- dockerfile/installNvidiaDriver.sh
+++ dockerfile/installNvidiaDriver.sh
@@ -21,3 +21,58 @@ sudo ${filename}
 # method 
 apt-get install nvidia-driver-515
 apt-get install nvidia-driver-550
+
+
+# ucloud
+sudo apt-get update
+sudo systemctl stop apt-daily.service
+sudo systemctl stop apt-daily.timer
+sudo systemctl stop apt-daily-upgrade.service
+sudo systemctl stop apt-daily-upgrade.timer
+sudo systemctl disable apt-daily.service
+sudo systemctl disable apt-daily.timer
+sudo systemctl disable apt-daily-upgrade.service
+sudo systemctl disable apt-daily-upgrade.timer
+sudo apt-mark hold linux-image-generic linux-headers-generic linux-image-$(uname -r) linux-headers-$(uname -r)
+apt-mark showhold
+
+
+sudo apt-get install libfuse2 libnl-3-dev libnl-route-3-dev tk -y
+sudo apt-get install gfortran libgfortran5 bison libnl-route-3-200 flex -y
+sudo apt-get install make net-tools unzip -y
+sudo wget http://security.ubuntu.com/ubuntu/pool/main/o/openssl/libssl1.1_1.1.1f-1ubuntu2_amd64.deb
+sudo dpkg -i libssl1.1_1.1.1f-1ubuntu2_amd64.deb
+
+os_version=ubuntu2204
+cuda_version=12.2.2
+driver_version=535.104.05
+nccl_version=2.18.5
+driver_main_version=$(echo $driver_version | awk -F '.' '{print $1}')
+base_developer_url=https://developer.download.nvidia.cn/compute
+cuda_main_version=${cuda_version:0:4}
+cuda_download_url=${base_developer_url}/cuda/${cuda_version}/local_installers/cuda_${cuda_version}_${driver_version}_linux.run
+nccl_download_url=${base_developer_url}/cuda/repos/${os_version}/x86_64/libnccl2_${nccl_version}-1+cuda${cuda_main_version}_amd64.deb
+nccl_dev_download_url=${base_developer_url}/cuda/repos/${os_version}/x86_64/libnccl-dev_${nccl_version}-1+cuda${cuda_main_version}_amd64.deb
+
+wget $nccl_download_url
+wget $nccl_dev_download_url
+wget $cuda_download_url
+
+
+cat >> /etc/modprobe.d/blacklist-nouveau.conf << EOF
+blacklist nouveau
+options nouveau modeset=0
+EOF
+
+
+sudo update-initramfs -u
+
+rmmod nouveau
+sudo sh cuda_${cuda_version}_${driver_version}_linux.run --silent --driver --toolkit
+sudo dpkg -i libncc*.deb
+
+
+cat >> ~/.bashrc << EOF
+export LD_LIBRARY_PATH=/usr/local/cuda/lib64
+export PATH=$PATH:/usr/local/cuda/bin
+EOF
